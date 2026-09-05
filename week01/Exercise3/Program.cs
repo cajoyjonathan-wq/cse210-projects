00dd460927162243f11c9643c05e9621a3136459
cajoyjonathan-wq <jcajoy@byupathway.edu>
2026-09-05 20:04:23 +0000

    while loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine("What is the magic number?");
+        Console.WriteLine("What is your guess?");
+
+        int guessNumber = int.Parse(Console.ReadLine());
+
+
+
+        while (guessNumber != number)
+        {
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber < number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.WriteLine("What is your guess?");
+
+            guessNumber = int.Parse(Console.ReadLine());
+
+        }
+        Console.Write("You guessed it!");
+
     }
 }
