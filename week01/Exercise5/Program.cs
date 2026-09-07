5ea2442055c4abad5ff7244a87112876959ae5b4
cajoyjonathan-wq <jcajoy@byupathway.edu>
2026-09-07 21:54:44 +1200

    Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int squaredNum = Squared(number);
+
+        Statement(name, squaredNum);
+
+    }
+    static int Squared(int number)
+    {
+        int sum = number * number;
+        return sum;
+    }
+    static void Statement(string name, int squared)
+    {
+        Console.Write($"{name}, the square of your number is {squared}.");
     }
 }
